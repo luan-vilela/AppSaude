3f2f43f11678020e23c1a31fccf5e67be286514d
luan vilela <luan.vilela.comp@gmail.com>
2019-06-08 08:48:57 -0400

    Construção class Connect. Contém todas as tabelas locais do usuário.

--- app/src/main/java/com/example/myapplication/model/Connect.java
+++ app/src/main/java/com/example/myapplication/model/Connect.java
@@ -7,38 +7,106 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Connect extends SQLiteOpenHelper {
 
 
-    private static final String DATABASE = "ConsultasOnline";
+    private static final String DATABASE = "database";
     private static final int VERSIOM_DATABASE = 1;
 
-    public static final String TABLE_USUARIO = "" +
-            "CREATE TABLE IF NOT EXISTS usuario (" +
-            "userId INTEGER PRIMARY KEY," +
-            "nomeUser VARCHAR(50) NOT NULL," +
-            "data_Nas VARCHAR(50) NOT NULL," +
-            "emailUser VARCHAR(80) NOT NULL," +
-            "sexo ENUM('Masculino', 'Feminino') NOT NULL," +
-            "gestante ENUM('Sim', 'Nao') NOT NULL," +
-            "telefoneUser VARCHAR(11)," +
-            "enderecoUser VARCHAR(80) NOT NULL," +
-            "cep INTEGER NOT NULL,"+
-            "numero INTEGER NOT NULL,"+
-            "bairro VARCHAR(50) NOT NULL," +
-            "complemento VARCHAR(50) NOT NULL," +")";
+    // Tabela profile
+    public static final String TABLE_PROFILE = "" +
+            "CREATE TABLE IF NOT EXISTS profile(" +
+            "id INTEGER PRIMARY KEY, " +
+            "name CHARACTER(100), " +
+            "email TEXT, " +
+            "sexo BOOLEAN, " +
+            "dataNascimento DATE, " +
+            "telefone TEXT, " +
+            "fotoCaminho TEXT, " +
+            "idRegistro TEXT, " +
+            "gestante BOOLEAN, " +
+            "idEndereco INTEGER, " +
+            "idData INTEGER, " +
+            "FOREIGN KEY(idEndereco) REFERENCES endereco(id), " +
+            "FOREIGN KEY(idData) REFERENCES data(id)" +
+            ")";
 
+    // Tabela data
+    public static final String TABLE_DATA = "CREATE TABLE IF NOT EXISTS  data(id INTEGER PRIMARY KEY, data DATETIME, local TEXT)";
+
+    // Tabela endereço
+    public static final String TABLE_ENDERECO = "" +
+            "CREATE TABLE IF NOT EXISTS endereco(" +
+            "id INTEGER PRIMARY KEY, " +
+            "rua TEXT, " +
+            "complemento TEXT, " +
+            "numero TEXT, " +
+            "bairro TEXT, " +
+            "codPost TEXT, " +
+            "provincia TEXT, " +
+            "pais TEXT" +
+            ")";
+
+    // Tabela Documento
+    public static final String TABLE_DOCUMENTO = "" +
+            "CREATE TABLE IF NOT EXISTS documento(" +
+            "id INTEGER PRIMARY KEY, " +
+            "nome TEXT, " +
+            "descricao TEXT, " +
+            "local_dispositivo TEXT, " +
+            "idData INTEGER, " +
+            "FOREIGN KEY(idData) REFERENCES data(id))";
+
+
+    // Tabela médico
+    public static final String TABLE_MEDICO = "" +
+            "CREATE TABLE IF NOT EXISTS medico(" +
+            "id INTEGER PRIMARY KEY, " +
+            "nome TEXT, " +
+            "especialidade TEXT, " +
+            "examesPedidos TEXT, " +
+            "observacao TEXT, " +
+            "gestante BOOLEAN, " +
+            "idData INTEGER, " +
+            "idProfile INTEGER, " +
+            "FOREIGN KEY(idData) REFERENCES data(id), " +
+            "FOREIGN KEY(idProfile) REFERENCES profile(id)" +
+            ")";
 
     public Connect(Context context) {
         super(context, DATABASE, null, VERSIOM_DATABASE);
     }
 
+    // Tabela Laudo
+    public static final String TABLE_LAUDO = "" +
+            "CREATE TABLE IF NOT EXISTS laudo(" +
+            "id INTEGER PRIMARY KEY, " +
+            "nome TEXT, " +
+            "local_dispositivo TEXT, " +
+            "gestante BOOLEAN, " +
+            "idData INTEGER, " +
+            "idProfile INTEGER, " +
+            "FOREIGN KEY(idData) REFERENCES data(id), " +
+            "FOREIGN KEY(idProfile) REFERENCES profile(id)" +
+            ")";
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TABLE_USUARIO);
+        // Criação das tabelas
+        db.execSQL(TABLE_PROFILE);
+        db.execSQL(TABLE_DATA);
+        db.execSQL(TABLE_ENDERECO);
+        db.execSQL(TABLE_DOCUMENTO);
+        db.execSQL(TABLE_MEDICO);
+        db.execSQL(TABLE_LAUDO);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion != newVersion){
-            db.execSQL("DROP TABLE IF EXISTS usuario");
+            db.execSQL("DROP TABLE IF EXISTS endereco");
+            db.execSQL("DROP TABLE IF EXISTS documento");
+            db.execSQL("DROP TABLE IF EXISTS medico");
+            db.execSQL("DROP TABLE IF EXISTS laudo");
+            db.execSQL("DROP TABLE IF EXISTS data");
+            db.execSQL("DROP TABLE IF EXISTS profile");
             onCreate(db);
         }
     }
